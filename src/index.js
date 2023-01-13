6e2ccaec8b6a3b6a6ffde1b19beeb11123099306
ja54312 <ija54312@gmail.com>
2023-01-13 17:52:09 -0600

    hello world con graphql

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const express = require("express");
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+  })
+);
 
 app.listen(3000, () => console.log("server on port 3000"));
---
+++ src/schema.js
@@ -0,0 +1,13 @@
+import { resolvers } from "./resolvers";
+import { makeExecutableSchema } from "graphql-tools";
+
+const typeDefs = `
+type Query{
+    hello: String
+}
+`;
+
+export default makeExecutableSchema({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
