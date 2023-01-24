4ba4fe1cfde00774d5c2f28e61717edaff69d339
ja54312 <ija54312@gmail.com>
2023-01-24 09:21:38 -0600

    se configuraron variables de ambiente

--- src/database.js
+++ src/database.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+const { config } = require("./config");
+
+const MONGO_URI = config.dbUrl;
 
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://root:root123@cluster-testing.jqptvdz.mongodb.net/graphql",
-      {
-        useNewUrlParser: true,
-      }
-    );
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    //console.log(config, "config");
     console.log(">>> Db is connected");
   } catch (error) {
     console.log("Algo salio mal", error);
